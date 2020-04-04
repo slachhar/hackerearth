398ef67968cf519a6b04ee0816d220fa628950b7
Shefali Lachhar <shefali.lachhar@gmail.com>
2020-04-04 11:32:51 +0530

    PalindromicString

---
+++ PalindromicString/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PalindromicString
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            string result = "YES";
+
+            for (int i = 0, j = input.Length - 1; i < input.Length / 2; i++, j--)
+            {
+                if (input[i] != input[j])
+                    result = "NO";
+            }
+
+            Console.WriteLine(result);
+            Console.ReadLine();
+        }
+    }
+}
