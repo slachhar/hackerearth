04694098da5e7176e2cb349c2bc337674bef792b
Shefali Lachhar <shefali.lachhar@gmail.com>
2020-04-06 11:28:05 +0530

    Best Index solved

---
+++ HackerEarthPractice/Best Index/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Best_Index
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int size = Convert.ToInt32(Console.ReadLine());
+            string[] inputArray = Console.ReadLine().Split(' ');
+            int[] numbers = new int[size];
+            int[] b = new int[size + 1];
+            int[] c = new int[size];
+            int maxSum = 0;
+
+            for (int i = 0; i < size; i++)
+            {
+                numbers[i] = Convert.ToInt32(inputArray[i]);
+            }
+
+            for (int i = 1; i <= size; i++)
+            {
+                b[i] = b[i - 1] + numbers[i - 1];
+            }
+
+            for (int i = 0; i < size; i++)
+            {
+
+                int j = i + 1; int demand = 2;
+
+                int remain = size - j;
+
+                while (demand <= remain && j < size)
+                {
+                    demand += 1;
+                    j = j + demand - 1;
+                    remain = size - j;
+                }
+
+                c[i] = b[j] - b[i];
+
+                if (maxSum < c[i])
+                    maxSum = c[i];
+            }
+
+            Console.WriteLine(maxSum);
+            Console.ReadLine();
+
+        }
+    }
+}
--- HackerEarthPractice/FindProduct/Program.cs
+++ HackerEarthPractice/FindProduct/Program.cs
@@ -10,7 +10,33 @@ namespace FindProduct
     {
         static void Main(string[] args)
         {
+            int x=0, y = 0;
+            string input = Console.ReadLine();
 
+            foreach (var item in input)
+            {
+                switch (item)
+                {
+                    case 'L':
+                        x--;
+                        break;
+
+                    case 'R':
+                        x++;
+                        break;
+
+                    case 'D':
+                        y--;
+                        break;
+
+                    case 'U':
+                        y++;
+                        break;
+                }
+            }
+
+            Console.WriteLine(x + " " + y);
+            Console.ReadLine();
         }
     }
 }
