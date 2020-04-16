562e700901b9b02758ed00c39b511b5cf1fba090
Shefali Lachhar <shefali.lachhar@gmail.com>
2020-04-16 15:59:57 +0530

    MonkAndWelcomeProblem

---
+++ DataStructures/Arrays/MonkAndWelcomeProblem/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MonkAndWelcomeProblem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int size = Convert.ToInt32(Console.ReadLine());
+
+            string[] array1 = Console.ReadLine().Split(' ');
+            string[] array2 = Console.ReadLine().Split(' ');
+
+            for (int i = 0; i < size; i++)
+            {
+                Console.Write((Convert.ToInt32(array1[i]) + Convert.ToInt32(array2[i])) + " ");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
