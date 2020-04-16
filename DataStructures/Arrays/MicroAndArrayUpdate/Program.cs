e01c6e9e5ec6be2edf2662b5828c6947eb17d783
Shefali Lachhar <shefali.lachhar@gmail.com>
2020-04-16 23:29:22 +0530

    MicroAndArrayUpdate

---
+++ DataStructures/Arrays/MicroAndArrayUpdate/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MicroAndArrayUpdate
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //StreamReader stream = new StreamReader("a2457f62fe-input-a245091.txt.clean.txt");
+            int numOfTestCases = Convert.ToInt32(Console.ReadLine());
+            int[] seconds = new int[numOfTestCases];
+
+            for (int i = 0; i < numOfTestCases; i++)
+            {
+                string[] nAndk = Console.ReadLine().Split(' ');
+                int arraySize = Convert.ToInt32(nAndk[0]);
+                int k = Convert.ToInt32(nAndk[1]);
+
+                int[] arrayDiff = new int[arraySize];
+
+                int index = 0;
+
+                foreach (var item in Console.ReadLine().Split(' '))
+                {
+                    arrayDiff[index] = k - Convert.ToInt32(item);
+                    index++;
+                }
+
+                seconds[i] = arrayDiff.Max() < 0 ? 0 : arrayDiff.Max();
+            }
+
+            foreach (var itemSecond in seconds)
+            {
+                Console.WriteLine(itemSecond);
+            }
+
+            Console.Read();
+        }
+    }
+}
